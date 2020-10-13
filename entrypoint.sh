7be61bf721f36277e961a784dc03e580f794ba16
Andreas Heil <info@aheil.de>
2020-10-13 23:05:24 +0200

    babysteps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -l
 
-echo "src_folder $1"
+echo "src-dir: $1"
+
+echo "ls: "
+
+ls /github/workspace/$1
+
+echo "files: "
 
 for f in /github/workspace/$1/*.md; do
     [ -f "$f" ] || break
