06844f318c140d9b97a4611bf8845a89c0fe964a
Andreas Heil <info@aheil.de>
2020-10-14 00:37:25 +0200

    babysteps

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh -l
 
+cd /github/workspace/$1
+
+repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git init
+git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git pull github ${GITHUB_REF} --ff-only
+
 for f in /github/workspace/$1/*.md; do
     [ -f "$f" ] || break
     fn=$(basename "$f" .md)
     echo "📄 Processing $fn ..."
     node /home/marp/.cli/marp-cli.js $f --allow-local-files -o "${fn}.pdf"
+    git add "${fn}.pdf"
 done
 
-cd /github/workspace/$1
-
-repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
-git init 
-
-git config user.name "${{ github.actor }}"
-git add *.pdf
 git status 
 git commit -m 'marp-action publish'
