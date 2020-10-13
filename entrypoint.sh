f57d77927888f9fadc9a658ad951c64c3d0f5a24
Andreas Heil <info@aheil.de>
2020-10-14 00:19:49 +0200

    added git

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,17 @@
 for f in /github/workspace/$1/*.md; do
     [ -f "$f" ] || break
     fn=$(basename "$f" .md)
+    echo "📄 Processing $fn ..."
     node /home/marp/.cli/marp-cli.js $f --allow-local-files -o "${fn}.pdf"
 done
+
+cd /github/workspace/$1
+
+repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git init 
+
+git config user.name "${{ github.actor }}"
+git add *.pdf
+git status 
+git commit -m 'marp-action publish'
